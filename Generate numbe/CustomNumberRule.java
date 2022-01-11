eed369b58ed768401b4db8982dd40934e64ef43e
tester911007 <60608141+tester911007@users.noreply.github.com>
2022-01-11 11:38:48 +0300

    Добавили проверки на ошибки и доработали код

--- Generate numbe/CustomNumberRule.java
+++ Generate numbe/CustomNumberRule.java
@@ -1,3 +1,6 @@
+package ru.ruselprom.tasks.rules;
+
+import org.apache.log4j.Logger;
 import wt.enterprise.EnterpriseHelper;
 import wt.iba.value.DefaultAttributeContainer;
 import wt.iba.value.IBAHolder;
@@ -5,6 +8,8 @@ import wt.iba.value.IBAValueUtility;
 import wt.iba.value.litevalue.AbstractValueView;
 import wt.iba.value.service.IBAValueHelper;
 import wt.inf.container.WTContainerRef;
+import wt.log4j.LogR;
+import wt.rule.algorithm.InvalidAlgorithmArgumentException;
 import wt.rule.algorithm.RuleAlgorithm;
 import wt.util.WTContext;
 import wt.util.WTException;
@@ -13,30 +18,44 @@ import java.rmi.RemoteException;
 import java.util.Locale;
 
 public class CustomNumberRule implements RuleAlgorithm {
-    @Override
-    public Object calculate(Object[] args, WTContainerRef wt_container_ref) {
-        String iba_name = "ИНДЕКС_ТС";
-        String iba_value = "";
-        String num = "ОШИБКА ГЕНЕРАЦИИ";
-        try {
-            num = EnterpriseHelper.getNumber(args);
-            String name;
+    private static final String RULE_RESOURCE = "wt.rule.ruleResource";
+    private static final Logger logger = LogR.getLogger("wt.content");
+    private static final String IBA_NAME = "ИНДЕКС_ТС";
 
+    @Override
+    public Object calculate(Object[] args, WTContainerRef wt_container_ref) throws WTException {
+        for (int symbol = 0; symbol < args.length; ++symbol) {
+            if (args[symbol] == null) {
+                throw new InvalidAlgorithmArgumentException("wt.rule.ruleResource", "387", new Object[]{symbol + 1});
+            }
+        }
+        String num = EnterpriseHelper.getNumber(args);
+
+        String iba_value = "";
+        try {
+            String attribute_name;
             IBAHolder obj_iba_holder = (IBAHolder) wt_container_ref.getObject();
             IBAHolder iba_holder = IBAValueHelper.service.refreshAttributeContainer(obj_iba_holder, null, WTContext.getContext().getLocale(), null);
             DefaultAttributeContainer default_attribute_container = (DefaultAttributeContainer) iba_holder.getAttributeContainer();
 
             for (AbstractValueView abstract_value_view : default_attribute_container.getAttributeValues()) {
-                name = abstract_value_view.getDefinition().getName();
-                if (iba_name.equalsIgnoreCase(name)) {
+                attribute_name = abstract_value_view.getDefinition().getName();
+                if (IBA_NAME.equalsIgnoreCase(attribute_name)) {
                     iba_value = IBAValueUtility.getLocalizedIBAValueDisplayString(abstract_value_view, Locale.getDefault());
                     break;
                 }
             }
         } catch (RemoteException | WTException e) {
-            e.printStackTrace();
+            logger.error("CustomNumberRule Error: " + e);
         }
 
-        return iba_value + "." + num;
+        if (iba_value.length() > 0) {
+            num = iba_value + "." + num;
+        }
+
+        return num;
+    }
+
+    public void validateArgs(String[] args) throws InvalidAlgorithmArgumentException {
     }
 }
